116f477524ceef44df88dc9a45a84f1b262c78bf
Grégory Marini <greg.marini@hotmail.fr>
2022-09-06 18:15:49 +0200

    installation react-router-dom mis en place du routage

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState, Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
     const [btn, setBtn] = useState(false);
@@ -34,14 +35,18 @@ const Landing = () => {
                 onMouseOut={clearImg}
                 className="leftBox"
             >
-                <button className="btn-welcome">Inscription</button>
+                <Link className="btn-welcome" to="/signup">
+                    Inscription
+                </Link>
             </div>
             <div
                 onMouseOver={setRightImg}
                 onMouseOut={clearImg}
                 className="rightBox"
             >
-                <button className="btn-welcome">Connexion</button>
+                <Link className="btn-welcome" to="/login">
+                    Connexion
+                </Link>
             </div>
         </Fragment>
     );
