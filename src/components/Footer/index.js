fa43853c19554c00b697e0909ae765480237cf11
Grégory Marini <greg.marini@hotmail.fr>
2022-09-06 14:10:29 +0200

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>
+                    Projet réalisé par Grégory Marini (Cours DonkeeGeek Udemy) -
+                    learn React -2022
+                </p>
+                <p>
+                    Les icônes Wolverine, Iron-man, Spider-man, Batman sont
+                    prises sur iconFinder.com
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Landing/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landing = () => {
+    return <main className="welcomePage">Test</main>;
+};
+
+export default Landing;
