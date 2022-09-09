7cd2ff715b278850504c5dc5d4dd9b42609248a3
Grégory Marini <greg.marini@hotmail.fr>
2022-09-09 15:53:06 +0200

    création formulaire de connexion avec firebase v9.9

---
+++ src/components/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const config = {
+    apiKey: "AIzaSyAOi4ZyH6ueN9YMmdWHX22x-NubK7XnKq0",
+    authDomain: "marvel-quizz-react-training.firebaseapp.com",
+    projectId: "marvel-quizz-react-training",
+    storageBucket: "marvel-quizz-react-training.appspot.com",
+    messagingSenderId: "563234700077",
+    appId: "1:563234700077:web:c98ab7f9f043507513b1a4",
+};
+
+const app = initializeApp(config);
+export const auth = getAuth(app);
