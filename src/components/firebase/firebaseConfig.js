5862f99e9dad65de5b6a84d639939da3cfbcc8db
Grégory Marini <greg.marini@hotmail.fr>
2022-09-26 15:53:43 +0200

    configuration avant déploiement

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, doc } from "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyAOi4ZyH6ueN9YMmdWHX22x-NubK7XnKq0",
-    authDomain: "marvel-quizz-react-training.firebaseapp.com",
-    projectId: "marvel-quizz-react-training",
-    storageBucket: "marvel-quizz-react-training.appspot.com",
-    messagingSenderId: "563234700077",
-    appId: "1:563234700077:web:c98ab7f9f043507513b1a4",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(config);
